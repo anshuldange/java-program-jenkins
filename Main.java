2f60f759930e0f811f29344d89cc6e93933522e6
anshuldange <165261215+anshuldange@users.noreply.github.com>
2024-11-21 10:58:33 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("This is java project");
+        System.out.println("From Main: hey Jenkins ");
+        System.out.println(new Date());
+    }
+
+}
